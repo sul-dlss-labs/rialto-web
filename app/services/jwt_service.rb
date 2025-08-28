a82a75e2425d6207470f9389bf187798ebafe717
Peter Mangiafico <peter@mangiafico.org>
2025-08-28 11:48:16 -0700

    jwt token embedding test

--- app/services/jwt_service.rb
+++ app/services/jwt_service.rb
@@ -2,20 +2,35 @@
 
 # Service for encoding JWT tokens
 class JwtService
+  ALGORITHM = 'HS256'
+
   def self.encode(groups)
+    new.encode(groups)
+  end
+
+  def encode(groups)
     # anyone who is logged in is part of the stanford group as far as tableau is concerned
     groups << 'stanford' unless groups.include?('stanford')
 
-    payload = { 'exp' => Time.now.to_i + Settings.tableau.token_expiry_time_seconds,
-                'jti' => SecureRandom.uuid,
-                'aud' => 'tableau',
-                'https://tableau.com/oda' => true,
-                'https://tableau.com/groups' => groups }
-    header =  { 'kid' => Settings.tableau.client_secret_id,
-                'iss' => Settings.tableau.client_id, 'alg' => 'HS256' }
-
-    token = JWT::Token.new(payload: payload, header: header)
-    token.sign!(algorithm: 'HS256', key: Settings.tableau.client_secret_value)
+    token = JWT::Token.new(payload: payload(groups), header: header)
+    token.sign!(algorithm: ALGORITHM, key: Settings.tableau.client_secret_value)
     token
+  end
+
+  private
+
+  def payload(groups)
+    { 'exp' => Time.now.to_i + Settings.tableau.token_expiry_time_seconds,
+      'jti' => SecureRandom.uuid,
+      'aud' => 'tableau',
+      'scp' => ['tableau:views:embed'],
+      'https://tableau.com/oda' => true,
+      'https://tableau.com/groups' => groups }
+  end
+
+  def header
+    { 'kid' => Settings.tableau.client_secret_id,
+      'iss' => Settings.tableau.client_id,
+      'alg' => ALGORITHM }
   end
 end
