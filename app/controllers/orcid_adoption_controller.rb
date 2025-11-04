3fe27f32bb9e82fd4910c3970b5080c707804d93
dnoneill <noneill@stanford.edu>
2025-11-04 17:44:57 -0500

    add authentication alerts, refactor authentication checks

--- app/controllers/orcid_adoption_controller.rb
+++ app/controllers/orcid_adoption_controller.rb
@@ -2,12 +2,9 @@
 
 # Controller for the orcid dashboards
 class OrcidAdoptionController < ApplicationController
-  before_action :mint_jwt_token, except: %i[show stanford_overview] # JWT only for views with dashboards behind auth
-  before_action :require_turbo_frame, except: [:show] # only turbo frame requests for embedded dashboards
+  before_action :require_turbo_frame, except: [:show]
 
-  # these views are public
-  allow_unauthenticated_access only: %i[show stanford_overview]
-  skip_verify_authorized only: %i[show stanford_overview]
+  skip_verify_authorized
 
   # main dashboard page, accessible to all users
   # use tab param to select which tab is active (default is overview)
@@ -19,24 +16,28 @@ class OrcidAdoptionController < ApplicationController
   # turbo frame loaded only when tab is selected
   def stanford_overview
     render DashboardEmbedComponent.new(embed_url: Settings.dashboard.orcid_adoption.stanford_overview,
-                                       turbo_frame_id: 'overview_tab')
+                                       turbo_frame_id: 'overview_tab', authorized: true)
   end
 
   # the schools and departments dashboard embedded view (stanford users only) -
   # turbo frame loaded only when tab is selected
   def schools_and_departments
-    authorize! to: :view?, with: StanfordPolicy
-
     render DashboardEmbedComponent.new(embed_url: Settings.dashboard.orcid_adoption.schools_and_departments,
-                                       turbo_frame_id: 'schools_tab', token: @token)
+                                       turbo_frame_id: 'schools_tab', token:,
+                                       authorized: current_user && allowed_to?(:view?, with: StanfordPolicy))
   end
 
   # the individual researchers dashboard embedded view (workgroup users only) -
   # turbo frame loaded only when tab is selected
   def individual_researchers
-    authorize! to: :view?, with: RestrictedPolicy
-
     render DashboardEmbedComponent.new(embed_url: Settings.dashboard.orcid_adoption.individual_researchers,
-                                       turbo_frame_id: 'researchers_tab', token: @token)
+                                       turbo_frame_id: 'researchers_tab', token:,
+                                       authorized: current_user && allowed_to?(:view?, with: RestrictedPolicy))
+  end
+
+  private
+
+  def token
+    mint_jwt_token if current_user
   end
 end
